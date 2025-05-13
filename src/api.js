2fef55059a827e110126721bd9e26b7aff9cb560
Nomy0608 <rgbnohemy@gmail.com>
2025-05-12 18:21:57 -0700

    BUSCA POR FAVOR

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const SearchImages = async(term) => {
-    const url = 'https://api.unsplash.com/search/photos/?client_id=Ojf-Ps4xZg7BsZsHOVPVUkQdjtJj0qfhVeSsdbs_hbY'
+    const url = 'https://api.unsplash.com/search/photos/?client_id=bbs1VzZnOfGf0Omdpny_1aUVVmpN5QCMTf4ZUpykkn8'
 
     const response = await axios.get(url, {
         params: {
